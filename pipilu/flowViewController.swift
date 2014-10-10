99f9156268214b1301ccd182b7354479ec2b277d
arkulo56 <arkulo@126.com>
2014-10-10 23:00:48 +0800

    2300

---
+++ pipilu/flowViewController.swift
@@ -0,0 +1,35 @@
+//
+//  flowViewController.swift
+//  pipilu
+//
+//  Created by 赵勇 on 14-10-10.
+//  Copyright (c) 2014年 赵勇. All rights reserved.
+//
+
+import UIKit
+
+class flowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
